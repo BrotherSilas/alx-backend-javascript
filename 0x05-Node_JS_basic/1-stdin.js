5287b53618751b00555c57c30eab526b2bcc147a
Silas Edet <silasedetsnr@gmail.com>
2025-01-05 02:28:28 -0800

    0x05-Node_JS_basic

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,14 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-process.stdin.on("data", (data) => {
-    const name = data.toString().trim();
-    console.log(`Your name is: ${name}`);
-    process.exit();
-});
-process.on("exit", () => {
-    console.log("This important software is now closing");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  
+  if (name) {
+    const trimmedName = name.toString().trim();
+    process.stdout.write(`Your name is: ${trimmedName}\n`);
+  }
 });
 
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
